3dbe9f423fbdeace13dfcba7f48cb30a59f409a7
Richard-post-ams <Fdbx-368956>
2021-10-13 16:06:14 +0200

    100 % of basic functionality implemented.

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ from urllib.request import Request, urlopen
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.offline as py
+import seaborn as sns
+import gc
+import warnings
+warnings.filterwarnings("ignore")
+from pandas_profiling import ProfileReport
+
+
 
 today = datetime.now()
 yesterday_str = "%s %d,%d" %(date.today().strftime("%b"), today.day-1, today.year)
@@ -75,6 +86,76 @@ df.columns = column_labels
 df["%Inc Cases"] = df["New Cases"]/df["Total Cases"]*100
 df["%Inc Deaths"] = df["New Deaths"]/df["Total Deaths"]*100
 df["%Inc Recovered"] = df["New Recovered"]/df["Total Recovered"]*100
+
+"""
+cases = df[["Total Recovered", "Active Cases", "Total Deaths"]].loc[0]
+#print(cases)
+cases_df = pd.DataFrame(cases).reset_index()
+cases_df.columns = ["Type", "Total"]
+cases_df["Percentage"] = np.round(100*cases_df['Total']/np.sum(cases_df["Total"]),2)
+cases_df["Virus"] = ["COVID-19" for i in range(len(cases_df))]
+"""
+
+cases = df[["New Cases", "New Recovered", "New Deaths"]].loc[0]
+#print(cases)
+cases_df = pd.DataFrame(cases).reset_index()
+cases_df.columns = ["Type", "Total"]
+cases_df["Percentage"] = np.round(100*cases_df['Total']/np.sum(cases_df["Total"]),2)
+cases_df["Virus"] = ["COVID-19" for i in range(len(cases_df))]
+
+per = np.round(df[["%Inc Cases", "%Inc Deaths", "%Inc Recovered"]].loc[0], 2)
+per_df = pd.DataFrame(per)
+per_df.columns = ["Percentage"]
+fig = go.Figure()
+fig.add_trace(go.Bar(x=per_df.index, y = per_df['Percentage'], marker_color = ["Yellow", "blue", "red"]))
+fig.show()
+
+fig = px.bar(cases_df, x = "Virus", y = "Percentage", color = "Type", hover_data=["Total"])
+fig.show()
+
+# PLOT PER CONTINENT
+continent_df = df.groupby("Continent").sum().drop("All")
+continent_df = continent_df.reset_index()
+#print(continent_df)
+
+def continent_visualization(vis_list):
+    for label in vis_list:
+        c_df = continent_df[['Continent', label]]
+        c_df['Percentage'] = np.round(100*c_df[label]/ np.sum(c_df[label]), 2)
+        c_df['Virus'] = ["COVID-19" for i in range (len(c_df))]
+        fig = px.bar(c_df, x="Virus", y="Percentage", color="Continent", hover_data=[label])
+        fig.update_layout(title={"text":f"{label}"})
+        fig.show()
+        gc.collect()
+
+cases_list = ["Total Cases", "Active Cases", "New Cases", "Serious/Critical", "Total Cases/1M"]
+deaths_list = ["Total Deaths", "New Deaths", ""]
+recovered_list = ["Total Recovered", "New Recovered", "%Inc Recovered"]
+
+print (continent_df)
+continent_visualization(cases_list)
+
+# PLOT PER COUNTRIES
+df = df.drop([len(df)-1])
+country_df = df.drop([0])
+# print(country_df)
+LOOK_AT = 5
+country = country_df.columns[1:14]
+fig = go.Figure()
+c=0
+for i in country_df.index:
+    if c < LOOK_AT:
+        fig.add_trace(go.Bar(name = country_df['Country'][i], x = country, y = country_df.loc[i][1:14]))
+    else:
+        break
+    c += 1
+fig.update_layout(title = {"text":f'top {LOOK_AT} countries affected'}, yaxis_type = "log")
+fig.show()
+
+
+
+
+
 
 
 
