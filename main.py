d36add15d208f9fdf087d100fc3d1ea8479afba6
Richard-post-ams <Fdbx-368956>
2021-10-15 16:52:53 +0200

    Adding more comments (80% complete)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 """
+Python version: 3.9.7
+Numpy version: 1.21.2
+Pandas version: 1.3.3
+Programmed by Richard Post
+
 Currently I am working with multiple software-(data) teams for a major American
 company in the Netherlands, but not as developer (I am working as a Scrum Master).
 Actually I want to move into (Python/Numpy/Pandas) coding, for Data Science.
@@ -25,7 +30,8 @@ Data Pre Processing
 EDA (Exploratory Data Analysis)
 Conclusions (did the analysis answer our questions, was there a limitation
 in our analysis which could affect our conclusion, was analysis sufficient
-enough in decision making)
+enough in decision making).
+
 """
 
 from bs4 import BeautifulSoup as soup #used to webscrape data from Worldometers
@@ -45,11 +51,15 @@ import warnings
 warnings.filterwarnings("ignore")
 from pandas_profiling import ProfileReport
 
+
 today = datetime.now() #to get the previous day data, which is the complete dataset
 yesterday_str = "%s %d,%d" %(date.today().strftime("%b"), today.day-1, today.year)
 #for debugging: 'print(yesterday_str)' should print the date of yesterday
 
-#Let's do some webscraping
+#===============================
+#WEBSCRAPING
+#===============================
+
 url = "https://www.worldometers.info/coronavirus/#countries"
 req = Request(url, headers={'User-Agent':"Mozilla/5.0"})
 webpage = urlopen(req)
@@ -104,13 +114,16 @@ for country in containers:
     all_data.append(country_data) #once out of the for loop all country_data will
     #be added to the list all_data
 #for debug: 'print all_data()' should print data for all countries as a list
+
 #now let's put the data in a form of a DataFrame, using Pandas.
 df = pd.DataFrame(all_data)
 
+#Remove columns with missing or incomplete data
 df.drop([15,16,17,18,19,20], inplace= True, axis =1)
-df.head()
 #For debugging: 'print(df.head())' should list the data in table form
+df.head()
 
+#Convert everything to numeric, except for Country and Continent
 for label in df.columns:
     if label != 'Country' and label != "Continent":
         df[label] = pd.to_numeric(df[label], errors='ignore')
@@ -118,14 +131,27 @@ for label in df.columns:
 
 #best way to get the column names is to scrape them with a for loop.
 #but for now I hardcode them (sorry.... :-))
-column_labels = ["Country", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered", "New Recovered", "Active Cases", "Serious/Critical", "Total Cases/1M", "Deaths/1M", "Total Tests", "Test/1M","Population", "Continent"]
+column_labels = ["Country", "Total Cases", "New Cases", "Total Deaths",
+                 "New Deaths", "Total Recovered", "New Recovered",
+                 "Active Cases", "Serious/Critical", "Total Cases/1M",
+                 "Deaths/1M", "Total Tests", "Test/1M","Population",
+                 "Continent"]
+
+#Show titles on top of our columns
 df.columns = column_labels
 
+#with Pandas Dataframes its very simple to calculate in table columns and rows
 df["%Inc Cases"] = df["New Cases"]/df["Total Cases"]*100
 df["%Inc Deaths"] = df["New Deaths"]/df["Total Deaths"]*100
 df["%Inc Recovered"] = df["New Recovered"]/df["Total Recovered"]*100
 
+#===============================
+#EDA (Exploratory Data Analysis)
+#===============================
+
 """
+#we use a DataFrame because we want to pick values, we use loc[0] because we need
+the value
 cases = df[["Total Recovered", "Active Cases", "Total Deaths"]].loc[0]
 #print(cases)
 cases_df = pd.DataFrame(cases).reset_index()
@@ -135,23 +161,38 @@ cases_df["Virus"] = ["COVID-19" for i in range(len(cases_df))]
 """
 
 cases = df[["New Cases", "New Recovered", "New Deaths"]].loc[0]
-#print(cases)
+#for debugging: print(cases)
+
+#Add cases into a DataFrame
 cases_df = pd.DataFrame(cases).reset_index()
+
+#Add "Type", "Total", "Percentage" and "Virus" to the Cases Dataframe.
 cases_df.columns = ["Type", "Total"]
 cases_df["Percentage"] = np.round(100*cases_df['Total']/np.sum(cases_df["Total"]),2)
 cases_df["Virus"] = ["COVID-19" for i in range(len(cases_df))]
+#for debugging: print(cases_df)
 
+# create a new Dataframe from the start (same explanation as above)
 per = np.round(df[["%Inc Cases", "%Inc Deaths", "%Inc Recovered"]].loc[0], 2)
 per_df = pd.DataFrame(per)
 per_df.columns = ["Percentage"]
+
+# For plotting we are using Plotly instead of Seaborn or Matplotlib,
+# because Plotly gives us a better visualization
 fig = go.Figure()
-fig.add_trace(go.Bar(x=per_df.index, y = per_df['Percentage'], marker_color = ["Yellow", "blue", "red"]))
+fig.add_trace(go.Bar(x=per_df.index, y = per_df['Percentage'],
+                     marker_color = ["Yellow", "blue", "red"]))
 fig.show()
 
-fig = px.bar(cases_df, x = "Virus", y = "Percentage", color = "Type", hover_data=["Total"])
+# use bar-graph, and pass dataset cases, x-axis, y-axis and color, and show "total"
+# when hovering over graph
+fig = px.bar(cases_df, x = "Virus", y = "Percentage", color = "Type",
+             hover_data=["Total"])
 fig.show()
 
+#===============================
 # PLOT PER CONTINENT
+#===============================
 continent_df = df.groupby("Continent").sum().drop("All")
 continent_df = continent_df.reset_index()
 #print(continent_df)
@@ -161,19 +202,24 @@ def continent_visualization(vis_list):
         c_df = continent_df[['Continent', label]]
         c_df['Percentage'] = np.round(100*c_df[label]/ np.sum(c_df[label]), 2)
         c_df['Virus'] = ["COVID-19" for i in range (len(c_df))]
-        fig = px.bar(c_df, x="Virus", y="Percentage", color="Continent", hover_data=[label])
+        fig = px.bar(c_df, x="Virus", y="Percentage", color="Continent",
+                     hover_data=[label])
         fig.update_layout(title={"text":f"{label}"})
         fig.show()
         gc.collect()
 
-cases_list = ["Total Cases", "Active Cases", "New Cases", "Serious/Critical", "Total Cases/1M"]
+cases_list = ["Total Cases", "Active Cases", "New Cases", "Serious/Critical",
+              "Total Cases/1M"]
 deaths_list = ["Total Deaths", "New Deaths", ""]
 recovered_list = ["Total Recovered", "New Recovered", "%Inc Recovered"]
 
 print (continent_df)
 continent_visualization(cases_list)
 
+#===============================
 # PLOT PER COUNTRY
+#===============================
+
 df = df.drop([len(df)-1])
 country_df = df.drop([0])
 # print(country_df)
@@ -183,11 +229,13 @@ fig = go.Figure()
 c=0
 for i in country_df.index:
     if c < LOOK_AT:
-        fig.add_trace(go.Bar(name = country_df['Country'][i], x = country, y = country_df.loc[i][1:14]))
+        fig.add_trace(go.Bar(name = country_df['Country'][i], x = country,
+                             y = country_df.loc[i][1:14]))
     else:
         break
     c += 1
-fig.update_layout(title = {"text":f'top {LOOK_AT} countries affected'}, yaxis_type = "log")
+fig.update_layout(title = {"text":f'top {LOOK_AT} countries affected'},
+                  yaxis_type = "log")
 fig.show()
 
 
